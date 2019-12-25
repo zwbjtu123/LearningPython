854b2089454b6f6883f8cd625d309c11a5a69081
Jesse <417873961@qq.com>
2019-12-25 15:19:01 +0800

    绘图最基础学习

---
+++ NumPyLearning/NumPyTest.py
@@ -0,0 +1,13 @@
+import numpy as np 
+x = np.array([1.0, 2.3, 3.0])
+print(x)
+print(type(x))
+
+x = np.array([1.0, 2.0, 3.0])
+y = np.array([2.0, 4.0, 6.0])
+z = x + y
+print(z)
+print(x - y)
+print(x*y)
+print(x/y)
+print(2*x)
